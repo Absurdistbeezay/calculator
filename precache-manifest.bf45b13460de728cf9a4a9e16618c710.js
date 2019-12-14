d0280300fff39a4f386ee64eff0b432bbaa415c4
Vijay Lama <b_jay05@hotmail.com>
2019-12-13 22:19:04 -0500

    Updates

---
+++ precache-manifest.bf45b13460de728cf9a4a9e16618c710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d79812835b068ea1101f85924acc47",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "aa19ce3afbf70d29118d",
+    "url": "/calculator/static/css/main.8e1e7bd7.chunk.css"
+  },
+  {
+    "revision": "191f4e828de12dd638ae",
+    "url": "/calculator/static/js/2.abfcd11f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.abfcd11f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa19ce3afbf70d29118d",
+    "url": "/calculator/static/js/main.ae5da16a.chunk.js"
+  },
+  {
+    "revision": "c3d9a3bbcb6bc1177d26",
+    "url": "/calculator/static/js/runtime-main.250539d0.js"
+  }
+]);
